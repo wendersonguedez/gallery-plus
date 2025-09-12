80261edf05b5863f6b5d21b6d3c1a5e6256a3bb5
wenderson <wendersonguedes6@gmail.com>
2025-09-11 23:25:33 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import Button from "./components/button";
+import ButtonIcon from "./components/button-icon";
+import ChevronLeftIcon from "./assets/icons/chevron-left.svg?react";
+import ChevronRightIcon from "./assets/icons/chevron-right.svg?react";
+import Badge from "./components/badge";
+import Alert from "./components/alert";
+import Divider from "./components/divider";
+import InputText from "./components/input-text";
+
+export default function App() {
+	return (
+		<div className="grid gap-7 p-6">
+			<div className="flex gap-3">
+				<Button>Button</Button>
+				<Button variant="secondary">Button</Button>
+				<Button disabled>Button</Button>
+				<Button handling>Loading</Button>
+				<Button icon={ChevronRightIcon}>Próxima Imagem</Button>
+				<Button variant="ghost" size="sm">
+					Button
+				</Button>
+				<Button variant="primary" size="sm">
+					Button
+				</Button>
+			</div>
+
+			<div className="flex gap-3">
+				<ButtonIcon icon={ChevronLeftIcon} />
+				<ButtonIcon icon={ChevronRightIcon} variant="secondary" />
+			</div>
+
+			<div className="flex gap-3">
+				<Badge>Todos</Badge>
+				<Badge>Natureza</Badge>
+				<Badge>Viagem</Badge>
+				<Badge loading>Viagem</Badge>
+				<Badge loading>Viagem</Badge>
+				<Badge loading>Viagem</Badge>
+			</div>
+
+			<div>
+				<Alert>
+					Tamanho máximo: 50MB
+					<br />
+					Você pode selecionar arquivos em PNG, JPG, JPEG ou WEBP
+				</Alert>
+			</div>
+
+			<div>
+				<Divider />
+			</div>
+		</div>
+	);
+}
