3651d658f038a33020fd6668fea8f9d7e50e598a
wenderson <wendersonguedes6@gmail.com>
2026-01-12 16:19:23 -0300

    feat: criado setup do react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
 import { BrowserRouter, Route, Routes } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import PageComponents from "@/pages/page-components";
 import LayoutMain from "@/pages/layout-main";
 import PageHome from "@/pages/page-home";
 import PagePhotoDetails from "@/pages/page-photo-details";
 
+const queryClient = new QueryClient();
+
 export default function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route element={<LayoutMain />}>
-					<Route index element={<PageHome />} />
-					<Route path="/photos/:id" element={<PagePhotoDetails />} />
-					{/**
-					 * Caso o usuário acesse a rota /components, o componente <PageComponents /> será renderizado
-					 * onde está o componente <Outlet />, definido no componente <LayoutMain />
-					 */}
-					<Route path="/components" element={<PageComponents />} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
+		/**
+		 * O Provider é necessário para que qualquer componente filho
+		 * possa acessar o cache e usar hooks como useQuery.
+		 */
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<Routes>
+					<Route element={<LayoutMain />}>
+						<Route index element={<PageHome />} />
+						<Route path="/photos/:id" element={<PagePhotoDetails />} />
+						{/**
+						 * Caso o usuário acesse a rota /components, o componente <PageComponents /> será renderizado
+						 * onde está o componente <Outlet />, definido no componente <LayoutMain />
+						 */}
+						<Route path="/components" element={<PageComponents />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</QueryClientProvider>
 	);
 }
