9b799a44890517aa5c91b1be982834d4d37d729e
wenderson <wendersonguedes6@gmail.com>
2026-01-14 00:27:12 -0300

    fix: removido filtro por texto no detalhe da foto

--- src/components/main-header.tsx
+++ src/components/main-header.tsx
@@ -4,13 +4,15 @@ import PhotosSearch from "@/components/photos-search";
 import Divider from "@/components/divider";
 import Logo from "@/assets/images/galeria-plus-full-logo.svg?react";
 import cx from "classnames";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import PhotoNewDialog from "@/contexts/photos/components/photo-new-dialog";
 import AlbumNewDialog from "@/contexts/albums/components/album-new-dialog";
 
 interface MainHeaderProps extends React.ComponentProps<typeof Container> {}
 
 export default function MainHeader({ className, ...props }: MainHeaderProps) {
+	const { pathname } = useLocation();
+
 	return (
 		<Container
 			as="header"
@@ -21,8 +23,12 @@ export default function MainHeader({ className, ...props }: MainHeaderProps) {
 				<Logo className="h-5" />
 			</Link>
 
-			<PhotosSearch />
-			<Divider orientation="vertical" className="h-10" />
+			{pathname === "/" && (
+				<>
+					<PhotosSearch />
+					<Divider orientation="vertical" className="h-10" />
+				</>
+			)}
 
 			<div className="flex items-center gap-3">
 				<PhotoNewDialog
