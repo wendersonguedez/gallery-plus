196529402d481e471fd6160fccd7ea1582140026
wenderson <wendersonguedes6@gmail.com>
2026-01-13 22:49:21 -0300

    feat: implementada lib nuqs para filtros

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { NuqsAdapter } from "nuqs/adapters/react-router/v7";
 import PageComponents from "@/pages/page-components";
 import LayoutMain from "@/pages/layout-main";
 import PageHome from "@/pages/page-home";
@@ -14,19 +15,21 @@ export default function App() {
 		 * possa acessar o cache e usar hooks como useQuery.
 		 */
 		<QueryClientProvider client={queryClient}>
-			<BrowserRouter>
-				<Routes>
-					<Route element={<LayoutMain />}>
-						<Route index element={<PageHome />} />
-						<Route path="/photos/:id" element={<PagePhotoDetails />} />
-						{/**
-						 * Caso o usuário acesse a rota /components, o componente <PageComponents /> será renderizado
-						 * onde está o componente <Outlet />, definido no componente <LayoutMain />
-						 */}
-						<Route path="/components" element={<PageComponents />} />
-					</Route>
-				</Routes>
-			</BrowserRouter>
+			<NuqsAdapter>
+				<BrowserRouter>
+					<Routes>
+						<Route element={<LayoutMain />}>
+							<Route index element={<PageHome />} />
+							<Route path="/photos/:id" element={<PagePhotoDetails />} />
+							{/**
+							 * Caso o usuário acesse a rota /components, o componente <PageComponents /> será renderizado
+							 * onde está o componente <Outlet />, definido no componente <LayoutMain />
+							 */}
+							<Route path="/components" element={<PageComponents />} />
+						</Route>
+					</Routes>
+				</BrowserRouter>
+			</NuqsAdapter>
 		</QueryClientProvider>
 	);
 }
